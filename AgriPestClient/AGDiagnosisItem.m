9d58b5658ebf15d745c9e6727bdbb3bfdf18fda9
sunwenyu <sunwenyu@bytedance.com>
2026-04-27 22:41:17 +0800

    chore: release version 1.0.114

---
+++ AgriPestClient/AGDiagnosisItem.m
@@ -0,0 +1,34 @@
+#import "AGDiagnosisItem.h"
+
+@implementation AGDiagnosisItem
+
+- (instancetype)init {
+  self = [super init];
+  if (self) {
+    // initialize property's default value, if any
+    
+  }
+  return self;
+}
+
+
+/**
+ * Maps json key to property name.
+ * This method is used by `JSONModel`.
+ */
++ (JSONKeyMapper *)keyMapper {
+  return [[JSONKeyMapper alloc] initWithModelToJSONDictionary:@{ @"diseaseName": @"diseaseName", @"confidence": @"confidence", @"controlPlan": @"controlPlan" }];
+}
+
+/**
+ * Indicates whether the property with the given name is optional.
+ * If `propertyName` is optional, then return `YES`, otherwise return `NO`.
+ * This method is used by `JSONModel`.
+ */
++ (BOOL)propertyIsOptional:(NSString *)propertyName {
+
+  NSArray *optionalProperties = @[@"diseaseName", @"confidence", @"controlPlan"];
+  return [optionalProperties containsObject:propertyName];
+}
+
+@end
---
+++ AgriPestClient/AGImageSizeDto.m
@@ -0,0 +1,34 @@
+#import "AGImageSizeDto.h"
+
+@implementation AGImageSizeDto
+
+- (instancetype)init {
+  self = [super init];
+  if (self) {
+    // initialize property's default value, if any
+    
+  }
+  return self;
+}
+
+
+/**
+ * Maps json key to property name.
+ * This method is used by `JSONModel`.
+ */
++ (JSONKeyMapper *)keyMapper {
+  return [[JSONKeyMapper alloc] initWithModelToJSONDictionary:@{ @"width": @"width", @"height": @"height" }];
+}
+
+/**
+ * Indicates whether the property with the given name is optional.
+ * If `propertyName` is optional, then return `YES`, otherwise return `NO`.
+ * This method is used by `JSONModel`.
+ */
++ (BOOL)propertyIsOptional:(NSString *)propertyName {
+
+  NSArray *optionalProperties = @[@"width", @"height"];
+  return [optionalProperties containsObject:propertyName];
+}
+
+@end
